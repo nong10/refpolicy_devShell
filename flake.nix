c44d795b8c8dbbde35ef428fdb85ccaaee21b754
nong10 <nong10w@outlook.com>
2024-08-24 10:21:27 +0200

    add packages

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "reference policy build & install environment";
+  description = "environment for selinux refpolicy";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -13,29 +13,38 @@
   {
     devShells."${system}".default = 
         pkgs.mkShell {
-        # create an environment with nodejs_18, pnpm, and yarn
           packages = with pkgs; [
-            gnumake
-            gnum4
-            python311
-            python311Packages.setuptools
-            (python311.withPackages ( ps: with ps; 
-              [ 
-                setuptools
-                (callPackage ./selinux_pypackage.nix {}) 
-              ]             # support/selinux_binary_policy_path.py
-            ))
-
+            gawk	      # awk
+            gnugrep	    # grep
+            coreutils	  # GNU coreutils: install sort
+            gnused	    # sed
+            gnumake	    # make
+            gnum4	      # m4
             policycoreutils # semodule
             checkpolicy     # checkpolicy checkmodule
             semodule-utils  # semodule_link semodule_unpackage semodule_expand semodule_package
             setools         # sechecker
             libselinux      # sefcontext_compile
             libxml2         # xmllint
+            (python311.withPackages ( ps: with ps; 
+              [ 
+                setuptools
+                (callPackage ./selinux_pypackage.nix {}) 
+              ]             # [policy_root]/support/selinux_binary_policy_path.py
+            ))
           ];
 
           shellHook = ''
             export TEST_VAR="Lmaoooooo"
+
+            export AWK="${pkgs.awk}/bin/awk" 
+            export GREP="${pkgs.gnugrep}/bin/grep" 
+            export INSTALL="${pkgs.coreutils}/bin/install" 
+            export SORT="${pkgs.coreutils}/bin/sort"
+            export M4="${pkgs.gnum4}/bin/m4" 
+            export PYTHON="${pkgs.python311}/bin/python3 -bb -t -t -E -W error"
+            export SED="${pkgs.gnused}/bin/sed" 
+
             export CHECKPOLICY="${pkgs.checkpolicy}/bin/checkpolicy" 
             export CHECKMODULE="${pkgs.checkpolicy}/bin/checkmodule"
             export SEMODULE="${pkgs.policycoreutils}/bin/semodule"
@@ -50,42 +59,8 @@
 
             export SECHECK="${pkgs.setools}/bin/sechecker"
             export XMLLINT="${pkgs.libxml2}/bin/xmllint"
-            echo "hello devShell! env for building and installing refpolicy" 
+            echo "hello devShell! env for refpolicy" 
           '';
-
-          #AWK ?= gawk
-          #GREP ?= grep -E
-          #INSTALL ?= install
-          #M4 ?= m4 -E -E
-          #PYTHON ?= python3 -bb -t -t -E -W error
-          #SED ?= sed
-          #SORT ?= LC_ALL=C sort
-          #UMASK ?= umask
         };
-
-#    defaultPackage."${system}" = 
-#
-#      #devShells."${system}".default = 
-#      let
-#        pkgs = import nixpkgs {inherit system; };
-#      in pkgs.mkShell {
-#        # create an environment with nodejs_18, pnpm, and yarn
-#        packages = with pkgs; [
-#          gnum4
-#          python39
-#          policycoreutils
-#          checkpolicy
-#        ];
-#
-#        shellHook = ''
-#          echo "hello!" 
-#        '';
-#
-#  #        export CHECKPOLICY="${pkgs.checkpolicy}/bin/checkpolicy" 
-#  #        export CHECKMODULE="${pkgs.checkpolicy}/bin/checkmodule"
-#  #        export SEMODULE="${pkgs.policycoreutils}/semodule"
-#  #        export THETESTVAR="Lmao"
-#  #      '';
-#    };
   };
 }
